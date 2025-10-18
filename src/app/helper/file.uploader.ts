b5a82c681dc18062685a1e496936fddff60b5e24
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-18 15:53:07 +0600

    Update: src/app/helper/file.uploader.ts

--- src/app/helper/file.uploader.ts
+++ src/app/helper/file.uploader.ts
@@ -5,7 +5,7 @@ import config from "../../config"
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(process.cwd(), "uploads"))
+    cb(null, path.join(process.cwd(), "/uploads"))
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)

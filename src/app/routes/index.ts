79236c789b0c248a9f4f91d568db5ea9d8263de9
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-18 21:18:40 +0600

    Update: src/app/routes/index.ts

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { userRoutes } from '../modules/user/user.route';
+import { authRoutes } from '../modules/auth/auth.routes';
 
 
 const router = express.Router();
@@ -9,6 +10,10 @@ const moduleRoutes = [
     {
         path: '/user',
         route: userRoutes
+    },
+    {
+        path: '/auth',
+        route: authRoutes
     }
 ];
 

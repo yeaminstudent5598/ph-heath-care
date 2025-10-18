15961b0ef01c84b5e457c2777ffa1b90df66ca36
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-18 21:18:38 +0600

    Update: src/app/modules/auth/auth.routes.ts

---
+++ src/app/modules/auth/auth.routes.ts
@@ -0,0 +1,10 @@
+import express from "express"
+import { AuthController } from "./auth.controller";
+const router = express.Router();
+
+router.post(
+    "/login",
+    AuthController.login
+)
+
+export const authRoutes = router;

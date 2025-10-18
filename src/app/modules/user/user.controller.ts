71083f1f5f84a13dd23b2b2c6e135913bbd72ffb
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-18 21:18:39 +0600

    Update: src/app/modules/user/user.controller.ts

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -4,13 +4,14 @@ import { UserService } from "./user.service";
 import sendResponse from "../../shared/sendResponse";
 
 const createPatient = catchAsync(async (req: Request, res: Response) => {
-//   const result = await UserService.createPatient(req.body);
-  console.log(req)
+  const result = await UserService.createPatient(req);
+  console.log(result)
+//   console.log(req)
   sendResponse(res, {
     statusCode: 201,
     success: true,
     message: "Patient created successfully!",
-    data: ""
+    data: result
   })
 })
 

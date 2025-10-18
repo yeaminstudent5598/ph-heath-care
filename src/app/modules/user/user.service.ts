32c97401c37903fe51e73ce8cb0ce49f7ac23e68
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-18 21:18:40 +0600

    Update: src/app/modules/user/user.service.ts

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -8,7 +8,7 @@ import { fileUploader } from "../../helper/file.uploader";
 const createPatient = async(req: Request) => {
     if (req.file) {
         const uploadResult = await fileUploader.uploadToCloudinary(req.file)
-
+        req.body.patient.profilePhoto= uploadResult?.secure_url
     }
 
  const hashPassword = await bcrypt.hash(req.body.password, 10);
@@ -16,16 +16,14 @@ const createPatient = async(req: Request) => {
  const result = await prisma.$transaction(async (tnx) => {
     await tnx.user.create({
         data:{
-            email: req.body.email,
-            password: hashPassword
+            email: req.body.patient.email,
+            password: hashPassword,
         }
     });
     return await tnx.patient.create({
         data: req.body.patient
     })
  })
-
- console.log(result)
  return result;
 }
 

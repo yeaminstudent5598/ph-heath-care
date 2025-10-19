71a81df0c0796d0bde9b9c5f4a5600e76cea5446
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-19 16:07:31 +0600

    Update: src/app/modules/user/user.controller.ts

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -5,7 +5,7 @@ import sendResponse from "../../shared/sendResponse";
 
 const createPatient = catchAsync(async (req: Request, res: Response) => {
   const result = await UserService.createPatient(req);
-  console.log(result)
+  // console.log(result)
 //   console.log(req)
   sendResponse(res, {
     statusCode: 201,
@@ -16,6 +16,42 @@ const createPatient = catchAsync(async (req: Request, res: Response) => {
 })
 
 
+const createAdmin = catchAsync(async (req: Request, res: Response) => {
+
+    const result = await UserService.createAdmin(req);
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: "Admin Created successfuly!",
+        data: result
+    })
+});
+
+const createDoctor = catchAsync(async (req: Request, res: Response) => {
+
+    const result = await UserService.createDoctor(req);
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: "Doctor Created successfuly!",
+        data: result
+    })
+});
+
+const getAllFormDB = catchAsync(async (req: Request, res: Response) => {
+  const { page, limit, searchTerm, sortBy, sortOrder} = req.query;
+  const result = await UserService.getAllFormDB({page: Number(page), limit: Number(limit), searchTerm, sortBy, sortOrder});
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: "User retrive successfully!",
+    data: result
+  })
+})
+
 export const UserController = {
-    createPatient
+    createPatient,
+    createAdmin,
+    createDoctor,
+    getAllFormDB,
 }

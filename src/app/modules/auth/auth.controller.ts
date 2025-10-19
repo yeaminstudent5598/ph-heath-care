cd56a6bac8f1fc26e44ed61ee5f246a7e78996d7
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-19 16:07:30 +0600

    Update: src/app/modules/auth/auth.controller.ts

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -5,13 +5,30 @@ import { AuthService } from "./auth.service";
 
 const login = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.login(req.body);
+  const {accessToken, refreshToken, needPasswordChange} = result;
+
+  res.cookie("accessToken", accessToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "none",
+    maxAge: 1000 * 60 * 60
+  })
+  res.cookie("refreshToken", refreshToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "none",
+    maxAge: 1000 * 60 * 60 * 24 * 90
+  })
+
   console.log(result)
 //   console.log(req)
   sendResponse(res, {
     statusCode: 201,
     success: true,
     message: "User Logged successfully!",
-    data: result
+    data: {
+      needPasswordChange
+    }
   })
 })
 

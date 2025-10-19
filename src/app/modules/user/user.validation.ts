6de926c0d820fe59d271f34517050bb6a71ded81
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-19 16:07:31 +0600

    Update: src/app/modules/user/user.validation.ts

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,3 +1,5 @@
+
+import { Gender } from "@prisma/client";
 import z from "zod";
 
 const createPatientValidationSchema = z.object({
@@ -9,6 +11,65 @@ const createPatientValidationSchema = z.object({
     })
 });
 
+
+const createAdminValidationSchema = z.object({
+    password: z.string({
+        error: "Password is required"
+    }),
+    admin: z.object({
+        name: z.string({
+            error: "Name is required!"
+        }),
+        email: z.string({
+            error: "Email is required!"
+        }),
+        contactNumber: z.string({
+            error: "Contact Number is required!"
+        })
+    })
+});
+
+const createDoctorValidationSchema = z.object({
+    password: z.string({
+        error: "Password is required"
+    }),
+    doctor: z.object({
+        name: z.string({
+            error: "Name is required!"
+        }),
+        email: z.string({
+            error: "Email is required!"
+        }),
+        contactNumber: z.string({
+            error: "Contact Number is required!"
+        }),
+        address: z.string().optional(),
+        registrationNumber: z.string({
+            error: "Reg number is required"
+        }),
+        experience: z.number().optional(),
+        gender: z.enum([Gender.MALE, Gender.FEMALE]),
+        appointmentFee: z.number({
+            error: "appointment fee is required"
+        }),
+        qualification: z.string({
+            error: "quilification is required"
+        }),
+        currentWorkingPlace: z.string({
+            error: "Current working place is required!"
+        }),
+        designation: z.string({
+            error: "Designation is required!"
+        })
+    })
+});
+
+
+
+
 export const UserValidation = {
-    createPatientValidationSchema
+    createPatientValidationSchema,
+    createAdminValidationSchema,
+    createDoctorValidationSchema,
+
 }

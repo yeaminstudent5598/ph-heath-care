428856175d80b39bea7b856e7691e3672deceea2
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-19 16:07:31 +0600

    Update: src/app/modules/user/user.route.ts

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,9 +2,16 @@ import express, { NextFunction, Request, Response } from 'express'
 import { UserController } from './user.controller'
 import { fileUploader } from '../../helper/file.uploader'
 import { UserValidation } from './user.validation'
+import { UserRole } from '@prisma/client'
+import auth from '../../middlewares/auth'
 
 const router = express.Router()
 
+
+router.get(
+    "/",
+    UserController.getAllFormDB
+)
 router.post(
     "/create-patient",
     fileUploader.upload.single('file'),
@@ -13,4 +20,26 @@ router.post(
         return UserController.createPatient(req, res, next)
     }   
 )
+
+
+router.post(
+    "/create-admin",
+    auth(UserRole.ADMIN),
+    fileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        req.body = UserValidation.createAdminValidationSchema.parse(JSON.parse(req.body.data))
+        return UserController.createAdmin(req, res, next)
+    }
+);
+
+router.post(
+    "/create-doctor",
+    auth(UserRole.ADMIN),
+    fileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        console.log(JSON.parse(req.body.data))
+        req.body = UserValidation.createDoctorValidationSchema.parse(JSON.parse(req.body.data))
+        return UserController.createDoctor(req, res, next)
+    }
+);
 export const userRoutes = router

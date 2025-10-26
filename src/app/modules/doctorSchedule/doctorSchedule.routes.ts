e6b9781ce5557840b982d5df4c97b917717ef0bf
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-26 12:28:31 +0600

    Update: src/app/modules/doctorSchedule/doctorSchedule.routes.ts

---
+++ src/app/modules/doctorSchedule/doctorSchedule.routes.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { DoctorScheduleController } from "./doctorSchedule.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
+
+const router = express.Router();
+
+router.post(
+    "/",
+    auth(UserRole.DOCTOR),
+    DoctorScheduleController.insertIntoDB
+)
+
+export const doctorScheduleRoutes = router;

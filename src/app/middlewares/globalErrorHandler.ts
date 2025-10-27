1fd51dd548dfa3434b8b589d2ff6742e76e7b0e0
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-27 10:57:37 +0600

    Update: src/app/middlewares/globalErrorHandler.ts

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,12 +1,47 @@
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express"
 import httpStatus from "http-status"
 
 const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-
-    let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    console.log(err)
+    let statusCode: number = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
     let success = false;
     let message = err.message || "Something went wrong!";
     let error = err;
+
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === "P2002") {
+            message = "Duplicate key error",
+                error = err.meta,
+                statusCode = httpStatus.CONFLICT
+        }
+        if (err.code === "P1000") {
+            message = "Authentication failed against database server",
+                error = err.meta,
+                statusCode = httpStatus.BAD_GATEWAY
+        }
+        if (err.code === "P2003") {
+            message = "Foreign key constraint failed",
+                error = err.meta,
+                statusCode = httpStatus.BAD_REQUEST
+        }
+    }
+
+    else if (err instanceof Prisma.PrismaClientValidationError) {
+        message = "Validation Error",
+            error = err.message,
+            statusCode = httpStatus.BAD_REQUEST
+    }
+    else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+        message = "Unknown Prisma error occured!",
+            error = err.message,
+            statusCode = httpStatus.BAD_REQUEST
+    }
+    else if (err instanceof Prisma.PrismaClientInitializationError) {
+        message = "Prisma client failed to initialize!",
+            error = err.message,
+            statusCode = httpStatus.BAD_REQUEST
+    }
 
     res.status(statusCode).json({
         success,

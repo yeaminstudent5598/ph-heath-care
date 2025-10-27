5c07f5fd8eef7b868874ac5a107dd5d96edd69c9
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-27 10:57:37 +0600

    Update: src/app/modules/doctor/doctor.controller.ts

---
+++ src/app/modules/doctor/doctor.controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import { IJWTPayload } from "../../types/common";
+import pick from "../../helper/pick";
+import { DoctorService } from "./doctor.service";
+import sendResponse from "../../shared/sendResponse";
+import { doctorFilterableFields } from "./doctor.constant";
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+    const fillters = pick(req.query, doctorFilterableFields)
+
+    const result = await DoctorService.getAllFromDB(fillters, options);
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Doctor fetched successfully!",
+        meta: result.meta,
+        data: result.data
+    })
+})
+
+const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
+
+    const { id } = req.params;
+
+    const result = await DoctorService.updateIntoDB(id, req.body);
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Doctor updated successfully!",
+        data: result
+    })
+})
+
+
+export const DoctorController = {
+    getAllFromDB,
+    updateIntoDB
+}

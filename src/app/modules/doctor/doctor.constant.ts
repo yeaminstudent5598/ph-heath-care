5af421848a31f69b4812f5d9879fa37814303a00
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-27 10:57:37 +0600

    Update: src/app/modules/doctor/doctor.constant.ts

---
+++ src/app/modules/doctor/doctor.constant.ts
@@ -0,0 +1,3 @@
+export const doctorFilterableFields = ["email", "contactNumber", "gender", "appointmentFee", "specialties", "searchTerm"]
+
+export const doctorSearchableFields = ["name", "email", "contactNumber"]

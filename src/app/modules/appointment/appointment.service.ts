0868114de964d3306daaf661d2424849b646a4ae
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-30 15:23:02 +0600

    Update: src/app/modules/appointment/appointment.service.ts

---
+++ src/app/modules/appointment/appointment.service.ts
@@ -0,0 +1,70 @@
+import { prisma } from "../../shared/prisma";
+import { IJWTPayload } from "../../types/common";
+import { v4 as uuidv4 } from 'uuid';
+
+const createAppointment = async (user: IJWTPayload, payload: { doctorId: string, scheduleId: string }) => {
+    const patientData = await prisma.patient.findUniqueOrThrow({
+        where: {
+            email: user.email
+        }
+    });
+
+    const doctorData = await prisma.doctor.findUniqueOrThrow({
+        where: {
+            id: payload.doctorId,
+            isDeleted: false
+        }
+    });
+
+    const isBookedOrNot = await prisma.doctorSchedules.findFirstOrThrow({
+        where: {
+            doctorId: payload.doctorId,
+            scheduleId: payload.scheduleId,
+            isBooked: false
+        }
+    })
+
+    const videoCallingId = uuidv4();
+
+    const result = await prisma.$transaction(async (tnx) => {
+        const appointmentData = await tnx.appointment.create({
+            data: {
+                patientId: patientData.id,
+                doctorId: doctorData.id,
+                scheduleId: payload.scheduleId,
+                videoCallingId
+            }
+        })
+
+        await tnx.doctorSchedules.update({
+            where: {
+                doctorId_scheduleId: {
+                    doctorId: doctorData.id,
+                    scheduleId: payload.scheduleId
+                }
+            },
+            data: {
+                isBooked: true
+            }
+        })
+
+        const transactionId = uuidv4();
+
+        await tnx.payment.create({
+            data: {
+                appointmentId: appointmentData.id,
+                amount: doctorData.appointmentFee,
+                transactionId
+            }
+        })
+
+        return appointmentData;
+    })
+
+
+    return result;
+};
+
+export const AppointmentService = {
+    createAppointment,
+};

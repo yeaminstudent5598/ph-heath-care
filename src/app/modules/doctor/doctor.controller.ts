90dbb330cde438e1855b23644d6110403201ecd7
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-30 15:23:02 +0600

    Update: src/app/modules/doctor/doctor.controller.ts

--- src/app/modules/doctor/doctor.controller.ts
+++ src/app/modules/doctor/doctor.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
+import { IJWTPayload } from "../../types/common";
 import pick from "../../helper/pick";
 import { DoctorService } from "./doctor.service";
 import sendResponse from "../../shared/sendResponse";
 import { doctorFilterableFields } from "./doctor.constant";
+import { ca } from "zod/v4/locales";
 
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
     const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
@@ -35,7 +37,55 @@ const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
 })
 
 
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await DoctorService.getByIdFromDB(id);
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Doctor retrieval successfully',
+        data: result,
+    });
+});
+
+const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await DoctorService.deleteFromDB(id);
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Doctor deleted successfully',
+        data: result,
+    });
+});
+
+
+const softDelete = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await DoctorService.softDelete(id);
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Doctor soft deleted successfully',
+        data: result,
+    });
+});
+
+const getAISuggestions = catchAsync(async (req: Request, res: Response) => {
+    const result = await DoctorService.getAISuggestions(req.body);
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'AI suggestions fetched successfully',
+        data: result,
+    });
+});
+
 export const DoctorController = {
     getAllFromDB,
-    updateIntoDB
+    updateIntoDB,
+    getByIdFromDB,
+    deleteFromDB,
+    softDelete,
+    getAISuggestions
 }

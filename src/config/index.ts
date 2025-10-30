a7f6ae3c4e87ec9f233eecfb593f1220afec0015
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-30 15:23:02 +0600

    Update: src/config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,5 +11,8 @@ export default {
         api_secret: process.env.CLOUDINARY_API_SECRET,
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY
-    }
+    },
+    openRouterApiKey: process.env.OPENROUTER_API_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
+
 }

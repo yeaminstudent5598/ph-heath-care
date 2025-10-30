7aeb44a06e709e633fb93f48b02ab77ad89c4868
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-30 15:23:02 +0600

    Update: src/app/modules/doctor/doctor.routes.ts

--- src/app/modules/doctor/doctor.routes.ts
+++ src/app/modules/doctor/doctor.routes.ts
@@ -1,14 +1,35 @@
 import express from "express";
 import { DoctorController } from "./doctor.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 const router = express.Router();
 
 router.get(
     "/",
     DoctorController.getAllFromDB
-)
+);
+
+router.post("/suggestion", DoctorController.getAISuggestions);
+
+router.get('/:id', DoctorController.getByIdFromDB);
 
 router.patch(
     "/:id",
+    auth(UserRole.ADMIN, UserRole.DOCTOR),
     DoctorController.updateIntoDB
-)
+);
+
+router.delete(
+    '/:id',
+    auth(UserRole.ADMIN),
+    DoctorController.deleteFromDB
+);
+
+router.delete(
+    '/soft/:id',
+    auth(UserRole.ADMIN),
+    DoctorController.softDelete);
+
+
+
 export const DoctorRoutes = router;

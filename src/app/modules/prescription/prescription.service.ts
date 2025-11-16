38c3c7be2cf00a0e205296cee5c2d6fa282ff681
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-16 14:52:36 +0600

    Update: src/app/modules/prescription/prescription.service.ts

---
+++ src/app/modules/prescription/prescription.service.ts
@@ -0,0 +1,85 @@
+import { AppointmentStatus, PaymentStatus, Prescription, UserRole } from "@prisma/client";
+import { IJWTPayload } from "../../types/common";
+import { prisma } from "../../shared/prisma";
+import ApiError from "../../errors/ApiError";
+import httpStatus from 'http-status'
+import { IOptions, paginationHelper } from "../../helper/paginationHelper";
+
+const createPrescription = async (user: IJWTPayload, payload: Partial<Prescription>) => {
+    const appointmentData = await prisma.appointment.findUniqueOrThrow({
+        where: {
+            id: payload.appointmentId,
+            status: AppointmentStatus.COMPLETED,
+            paymentStatus: PaymentStatus.PAID
+        },
+        include: {
+            doctor: true
+        }
+    })
+
+    if (user.role === UserRole.DOCTOR) {
+        if (!(user.email === appointmentData.doctor.email))
+            throw new ApiError(httpStatus.BAD_REQUEST, "This is not your appointment")
+    }
+
+    const result = await prisma.prescription.create({
+        data: {
+            appointmentId: appointmentData.id,
+            doctorId: appointmentData.doctorId,
+            patientId: appointmentData.patientId,
+            instructions: payload.instructions as string,
+            followUpDate: payload.followUpDate || null
+        },
+        include: {
+            patient: true
+        }
+    });
+
+    return result;
+}
+
+const patientPrescription = async (user: IJWTPayload, options: IOptions) => {
+    const { limit, page, skip, sortBy, sortOrder } = paginationHelper.calculatePagination(options);
+
+    const result = await prisma.prescription.findMany({
+        where: {
+            patient: {
+                email: user.email
+            }
+        },
+        skip,
+        take: limit,
+        orderBy: {
+            [sortBy]: sortOrder
+        },
+        include: {
+            doctor: true,
+            patient: true,
+            appointment: true
+        }
+    })
+
+    const total = await prisma.prescription.count({
+        where: {
+            patient: {
+                email: user.email
+            }
+        }
+    })
+
+    return {
+        meta: {
+            total,
+            page,
+            limit
+        },
+        data: result
+    }
+
+};
+
+
+export const PrescriptionService = {
+    createPrescription,
+    patientPrescription
+}

8ea35255a62d154d54c4430b3185027d1bb5a700
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-16 14:52:33 +0600

    Update: src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,29 @@ import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 import config from './config';
 import router from './app/routes';
+import cookieParser from 'cookie-parser'
+import { PaymentController } from './app/modules/payment/payment.controller';
+
 
 const app: Application = express();
+
+app.post(
+    "/webhook",
+    express.raw({ type: "application/json" }),
+    PaymentController.handleStripeWebhookEvent
+);
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3001',
     credentials: true
 }));
 
 //parser
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1', router)
-    
+app.use("/api/v1", router);
+
 app.get('/', (req: Request, res: Response) => {
     res.send({
         message: "Server is running..",
@@ -25,7 +35,6 @@ app.get('/', (req: Request, res: Response) => {
         timeStamp: new Date().toISOString()
     })
 });
-
 
 app.use(globalErrorHandler);
 

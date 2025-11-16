be8ea2877a159242f0bebc83baa3c5be89b12f8d
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-16 14:52:33 +0600

    Update: src/app/modules/appointment/appointment.controller.ts

--- src/app/modules/appointment/appointment.controller.ts
+++ src/app/modules/appointment/appointment.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from "../../shared/catchAsync";
 import { AppointmentService } from "./appointment.service";
 import sendResponse from "../../shared/sendResponse";
 import { IJWTPayload } from "../../types/common";
+import pick from "../../helper/pick";
 
 
 const createAppointment = catchAsync(async (req: Request & { user?: IJWTPayload }, res: Response) => {
@@ -17,6 +18,38 @@ const createAppointment = catchAsync(async (req: Request & { user?: IJWTPayload 
     })
 });
 
+const getMyAppointment = catchAsync(async (req: Request & { user?: IJWTPayload }, res: Response) => {
+    const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+    const fillters = pick(req.query, ["status", "paymentStatus"])
+    const user = req.user;
+    const result = await AppointmentService.getMyAppointment(user as IJWTPayload, fillters, options);
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Appointment fetched successfully!",
+        data: result
+    })
+})
+
+
+const updateAppointmentStatus = catchAsync(async (req: Request & { user?: IJWTPayload }, res: Response) => {
+    const { id } = req.params;
+    const { status } = req.body;
+    const user = req.user;
+
+    const result = await AppointmentService.updateAppointmentStatus(id, status, user as IJWTPayload);
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Appointment updated successfully!",
+        data: result
+    })
+})
+
 export const AppointmentController = {
     createAppointment,
+    getMyAppointment,
+    updateAppointmentStatus
 }

bc0995898ca8232891d976bd08c695ac7a43ee27
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-16 14:52:33 +0600

    Update: src/app/modules/appointment/appointment.routes.ts

--- src/app/modules/appointment/appointment.routes.ts
+++ src/app/modules/appointment/appointment.routes.ts
@@ -5,10 +5,22 @@ import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
+router.get(
+    "/my-appointments",
+    auth(UserRole.PATIENT, UserRole.DOCTOR),
+    AppointmentController.getMyAppointment
+)
+
 router.post(
     "/",
     auth(UserRole.PATIENT),
     AppointmentController.createAppointment
+)
+
+router.patch(
+    "/status/:id",
+    auth(UserRole.ADMIN, UserRole.DOCTOR),
+    AppointmentController.updateAppointmentStatus
 )
 
 export const AppointmentRoutes = router;

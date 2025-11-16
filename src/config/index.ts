46103fa3f5c6989a31466d07f6b755fe18503828
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-16 14:52:36 +0600

    Update: src/config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,6 +13,19 @@ export default {
         api_key: process.env.CLOUDINARY_API_KEY
     },
     openRouterApiKey: process.env.OPENROUTER_API_KEY,
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY
-
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    emailSender: {
+        email: process.env.EMAIL,
+        app_pass: process.env.APP_PASS
+    },
+    jwt: {
+        jwt_secret: process.env.JWT_SECRET,
+        expires_in: process.env.EXPIRES_IN,
+        refresh_token_secret: process.env.REFRESH_TOKEN_SECRET,
+        refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN,
+        reset_pass_secret: process.env.RESET_PASS_TOKEN,
+        reset_pass_token_expires_in: process.env.RESET_PASS_TOKEN_EXPIRES_IN
+    },
+    salt_round: process.env.SALT_ROUND,
+    reset_pass_link: process.env.RESET_PASS_LINK
 }
